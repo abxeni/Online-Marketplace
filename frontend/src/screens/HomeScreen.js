1209fea159da845678045f86af4f94d7447c803c
abxeni <ihba784ne@gmail.com>
2021-10-23 15:22:41 -0600

    "BackEnd Product Fetch Done"

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const Homescreen = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const {data} = await axios.get('/api/products')
+
+            setProducts(data)   
+        }
+        
+        fetchProducts()
+    },[])
+
+
     return (
         <div>
-            <h2>Latest Products</h2>
+            <h2>Latest Products</h2>    
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
