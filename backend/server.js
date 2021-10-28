80984e28d2417534bcc1fb02ad67c486a128d5a6
abxeni <ihba784ne@gmail.com>
2021-10-28 15:04:39 -0600

    MongoDB connection added.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'  
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Request receieved......')
+    res.send('API is working...........')
 })
 
 app.get('/api/products', (req, res) => {
@@ -16,4 +23,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port::5000'))
+const PORT = process.env.PORT || 50000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV}on port::${PORT}`.yellow.bold))
